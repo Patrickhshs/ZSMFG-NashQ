2ba10a4c47a9a4ecb024bd251f073a82c68a7c67
Patrickhshs <js12556@nyu.edu>
2023-09-05 14:48:26 +0400

    feat

--- ZSMFG-NashQ/ZSMFG-SimpleModelTabularNashQ/plots/plot_convergence.py
+++ ZSMFG-NashQ/ZSMFG-SimpleModelTabularNashQ/plots/plot_convergence.py
@@ -11,6 +11,9 @@ npzfile = np.load("ZSMFG-NashQ/historyTables/corrected/Q_MC_zeros_ecos_2action_r
 n_states_x =    npzfile['n_states_x']
 n_steps_state = npzfile['n_steps_state']
 n_steps_ctrl =  npzfile['n_steps_ctrl']
+Q_1_visited_times = npzfile["Q_1_visited"]
+Q_2_visited_times = npzfile["Q_2_visited"]
+
 iters =         [i for i in range(1,5001)]
 Q_1_diff_sup  =    np.log(npzfile['Q_1_diff_sup'])
 Q_1_diff_L2  =    np.log(npzfile['Q_1_diff_L2'])
@@ -25,13 +28,16 @@ Q_2_diff_L2  =    np.log(npzfile['Q_2_diff_L2'])
 
 if __name__ == "__main__":
     #print(Q_1.shape)
-    plt.clf()
+    print(np.average(Q_1_visited_times))
+    print(np.average(Q_2_visited_times))
+
+    # plt.clf()
     # plt.plot(iters, Q_1_diff_sup, label='Q_1_diff_sup',marker="s",linestyle="--")
     # plt.plot(iters, Q_1_diff_L2, label='Q_1_diff_L2',marker="o")
-    plt.plot(iters, Q_2_diff_sup, label='Q_2_diff_sup',marker="d",linestyle="--")
-    plt.plot(iters, Q_2_diff_L2, label='Q_2_diff_L2',marker="x")
-    plt.xlabel('iterations')
-    plt.ylabel('log-scale')
-    plt.legend()
-    plt.title("plot of player2's convergence result")
-    plt.savefig("ZSMFG-NashQ/ZSMFG-SimpleModelTabularNashQ/plots/convergenceQ2_2actions_zeros_MC_5000iters_L2.png")
+    # # plt.plot(iters, Q_2_diff_sup, label='Q_2_diff_sup',marker="d",linestyle="--")
+    # # plt.plot(iters, Q_2_diff_L2, label='Q_2_diff_L2',marker="x")
+    # plt.xlabel('iterations')
+    # plt.ylabel('log-scale')
+    # plt.legend()
+    # plt.title("plot of player1's convergence result")
+    # plt.savefig("ZSMFG-NashQ/ZSMFG-SimpleModelTabularNashQ/plots/convergenceQ1_2actions_zeros_MC_5000iters_L2.pdf")
