ef35a2478e69a40e3ce6181d86cb756351829acf
Patrickhshs <js12556@nyu.edu>
2023-09-30 05:34:40 -0400

    feat

---
+++ ZSMFG-NashQ/ZSMFG-DLNashQ/plot.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+#npzfile = np.load("ZSMFG-NashQ/historyTables/Q_1_and_Q_2_results_iter1000.npz")
+npzfile = np.load("ZSMFG-NashQ/ZSMFG-DLNashQ/nashDQNmodels/training_loss.npz")
+#Q =             npzfile['Q_1_diff_sup']
+loss_1 =    npzfile['player_1_loss']
+loss_2 =    npzfile['player_2_loss']
+
+
+iters =         [i for i in range(len(loss_1))]
+
+
+# Q_1_diff_sup  =    npzfile['Q_1_diff_sup']
+# Q_1_diff_L2  =    npzfile['Q_1_diff_L2']
+# Q_2_diff_sup  =    npzfile['Q_2_diff_sup']
+# Q_2_diff_L2  =    npzfile['Q_2_diff_L2']
+# Q_1 = npzfile["Q_1"]
+
+if __name__ == "__main__":
+    #print(Q_1.shape)
+
+    plt.clf()
+    #plt.plot(iters, loss_1, label='Q_1_diff_sup',marker="s",linestyle="--")
+    plt.plot(iters, loss_2, label='Player_1_training_loss')#,marker="o")
+    # plt.plot(iters, Q_2_diff_sup, label='Q_2_diff_sup',marker="d",linestyle="--")
+    # plt.plot(iters, Q_2_diff_L2, label='Q_2_diff_L2',marker="x")
+    plt.xlabel('iterations')
+    plt.ylabel('log-scale')
+    plt.legend()
+    plt.title("plot of player 2's convergence result")
+    plt.savefig("ZSMFG-NashQ/ZSMFG-DLNashQ/plots/loss_1.pdf")
